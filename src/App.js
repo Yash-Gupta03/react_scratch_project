8bca2dbebac12f6792d805f55352d098eef86bfb
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-11-10 17:34:58 +0530

    props introduced

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const data = [{
+    title: 'books', amount: 1000, location: 'Indore'
+  }, 
+{
+  title: 'travel', amount: 3000, location: 'Indore'
+}, {
+  title: 'adventure', amount: 10000, location: 'Himalaya'
+}]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+
+      <ExpenseItem title={data[0].title} amount = {data[0].amount} location = {data[0].location}></ExpenseItem>
+      <ExpenseItem title={data[1].title} amount = {data[1].amount} location = {data[1].location}></ExpenseItem>
+      <ExpenseItem title={data[2].title} amount = {data[2].amount} location = {data[2].location}></ExpenseItem>
+      
+      {/* for (let i=0; i < 3; i++){
+        <ExpenseItem title={data[i].title} amount = {data[i].amount} location = {data[i].location}></ExpenseItem>
+      } */}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2023, 11, 10);
-    const expenseItem = 'Grocery';
-    const expenseAmount = 5000;
-    const location = 'Indore'
+    // const expenseDate = new Date(2023, 11, 10);
+    // const expenseItem = 'Grocery';
+    // const expenseAmount = 5000;
+    // const location = 'Indore'
 
     return (
         <div>
-        <div><h2>{expenseDate}</h2></div>
-        <div><h4>{expenseItem}</h4></div>
-        <div><h4>{expenseAmount}</h4></div>
-        <div><h4>{location}</h4></div>
+        <h4>{props.title}</h4>
+        <h4>{props.amount}</h4>
+        <h4>{props.location}</h4>
         </div>
     )
 }
