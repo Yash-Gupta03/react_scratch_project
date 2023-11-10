252d05ac9f301ab487bdf0410073400c1813d1ac
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-11-10 12:37:45 +0530

    used js variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 function ExpenseItem() {
+
+    const expenseDate = new Date(2023, 11, 10);
+    const expenseItem = 'Grocery';
+    const expenseAmount = 5000;
+    const location = 'Indore'
+
     return (
         <div>
-        <h2>Expense Items</h2>
-        <h4>Food Rs 10</h4>
-        <h4>Petrol Rs 100</h4>
-        <h4>Movies Rs 200</h4>
+        <div><h2>{expenseDate}</h2></div>
+        <div><h4>{expenseItem}</h4></div>
+        <div><h4>{expenseAmount}</h4></div>
+        <div><h4>{location}</h4></div>
         </div>
     )
 }
