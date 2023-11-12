761b3e9c7d05a68688f56e09f934775b03021b8d
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-11-12 12:57:19 +0530

    State changed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
@@ -8,18 +9,27 @@ const ExpenseItem = (props) => {
     // const expenseAmount = 5000;
     // const location = 'Indore'
 
+    const [title, setTitle] = useState(props.title);
+
+    const [amount, setAmount] = useState(props.amount);
+
     const deleteExpense = () => {
-        console.log('This is to delete a expense');
+        setTitle('new Title')
+        console.log(title);
     }
     
+    const ExpenseAmount = () => {
+        setAmount(5000);
+    }
 
     return (
         <div className='expense-item'>
         <ExpenseDate date= {props.date}/>
-        <div className='expense-item__description'>{props.title}</div>
+        <div className='expense-item__description'>{title}</div>
         <div className='expense-item__description'>{props.location}</div>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>{amount}</div>
         <button onClick={deleteExpense}>Delete</button>
+        <button onClick={ExpenseAmount}>Amount</button>
         </div>
     )
 }
